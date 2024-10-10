a48585ecb6300b4c9aea53fcdb5decf73aaca38e
Martí Climent <marticlilop@gmail.com>
2024-10-10 17:09:00 +0200

    Script improvements

--- solver.py
+++ solver.py
@@ -1,10 +1,10 @@
 import random, time
 
-NUM_OF_DIGITS = 10 # Minimum 4, maximum depends on your CPU
-REPS = 100000 # Around 500 with pypy, around 200 with regular python
+NUM_OF_DIGITS = 10   
+REPS = 1000             # Around 500 with pypy, around 200 with regular python
 PRINT_EACH_RESULT = False
 
-class PLAY:
+class Guess:
     N1: int = 0
     N2: int = 0
     N3: int = 0
@@ -14,7 +14,7 @@ class PLAY:
         pass
     
     def FromString(string: str):
-        self = PLAY()
+        self = Guess()
         self.N1 = int(string[0])
         self.N2 = int(string[1])
         self.N3 = int(string[2])
@@ -22,7 +22,7 @@ class PLAY:
         return self
         
     def FromInt(x: int, y: int, z: int, w: int):
-        self = PLAY()
+        self = Guess()
         self.N1 = x
         self.N2 = y
         self.N3 = z
@@ -30,11 +30,16 @@ class PLAY:
         return self
     
     def FromRandomGen():
-        self = PLAY()
-        self.N1 = random.randint(0, NUM_OF_DIGITS-1)
-        self.N2 = random.randint(0, NUM_OF_DIGITS-1)
-        self.N3 = random.randint(0, NUM_OF_DIGITS-1)
-        self.N4 = random.randint(0, NUM_OF_DIGITS-1)
+        self = Guess()
+        self.N1 = self.N2 = self.N3 = self.N4 = random.randint(0, NUM_OF_DIGITS-1)
+        
+        while self.N1 == self.N2:
+            self.N2 = random.randint(0, NUM_OF_DIGITS-1)
+        while self.N1 == self.N3 or self.N2 == self.N3: 
+            self.N3 = random.randint(0, NUM_OF_DIGITS-1)
+        while self.N1 == self.N4 or self.N2 == self.N4 or self.N3 == self.N4: 
+            self.N4 = random.randint(0, NUM_OF_DIGITS-1)
+        
         return self
          
     def __str__(self) -> str:
@@ -46,111 +51,95 @@ class PLAY:
     def compactString(self) -> str:
         return f"{self.N1}{self.N2}{self.N3}{self.N4}"
     
-bannedNumbers: list[list[int]] = [[], [], [], []]
-playedCombis: list[PLAY] = []
-playedResults: list[tuple[int, int]] = []
+    def __eq__(self, other: "Guess") -> bool:
+        return other.N1 == self.N1 and other.N2 == self.N2 and other.N3 == self.N3 and other.N4 == self.N4
+
+class Result:
+    Guess: Guess
+    CorrectNums: int
+    TotalCoincidences: int
+    
+    def IsCompatible(self, other: 'Guess') -> bool:
+        if self.GetTotalCoincidences(other) != self.TotalCoincidences: return False
+        if self.GetCorrectNums(other) != self.CorrectNums: return False
+        return True
+    
+    def GetTotalCoincidences(self, other: 'Guess') -> int:
+        return int(self.Guess.contains(other.N1)) + \
+               int(self.Guess.contains(other.N2)) + \
+               int(self.Guess.contains(other.N3)) + \
+               int(self.Guess.contains(other.N4))
+    
+    def GetCorrectNums(self, other: 'Guess') -> int:
+        return int(self.Guess.N1 == other.N1) + \
+               int(self.Guess.N2 == other.N2) + \
+               int(self.Guess.N3 == other.N3) + \
+               int(self.Guess.N4 == other.N4)
 
 
-def getRandomNumber() -> str:
-    num: PLAY = PLAY.FromRandomGen()
-    while(num.N1 == num.N2 or num.N1 == num.N3 or num.N1 == num.N4 or num.N2 == num.N3 or num.N2 == num.N4 or num.N3 == num.N4\
-        or num.N1 in bannedNumbers[0] or num.N2 in bannedNumbers[1] or num.N3 in bannedNumbers[2] or num.N4 in bannedNumbers[3]):
-        num = PLAY.FromRandomGen()
-                
-    return num
 
-PLAYS: list[PLAY] = []
-RESULTS: list[tuple[int, int]] = []
-
-
-def getPossibleCombinations() -> list[PLAY]:    
-    nums: list[PLAY] = []
+def GetPossibleCombinations() -> list[Guess]:    
+    nums: list[Guess] = []
     for x in range(0, NUM_OF_DIGITS):
         for y in range(0, NUM_OF_DIGITS):
             for z in range(0, NUM_OF_DIGITS):
                 for w in range(0, NUM_OF_DIGITS):
                     if (x == y or x == z or x == w or y == z or y == w or z == w): continue
-                    if (x in bannedNumbers[0]): continue
-                    if (y in bannedNumbers[1]): continue
-                    if (z in bannedNumbers[2]): continue
-                    if (w in bannedNumbers[3]): continue
-                    
-                    num = PLAY.FromInt(x, y, z, w)                                        
+                    num = Guess.FromInt(x, y, z, w)                                        
                     nums.append(num)
-            
     return nums
 
-def GetWrongPosCount(a: PLAY, b: PLAY) -> int:
-    return (1 if a.contains(b.N1) else 0) + (1 if a.contains(b.N2) else 0) + (1 if a.contains(b.N3) else 0) + (1 if a.contains(b.N4) else 0)
-    
-def GetCorrectNumCount(a: PLAY, b: PLAY) -> int:
-    return (1 if a.N1 == b.N1 else 0) + (1 if a.N2 == b.N2 else 0) + (1 if a.N3 == b.N3 else 0) + (1 if a.N4 == b.N4 else 0)
-    
-def getEqualOrBetterCombinations(combis: list[PLAY], current: PLAY, jaques, mates) -> list[PLAY]:
-    if(jaques + mates == 0):
-        bannedNumbers[0].append(current.N1)
-        bannedNumbers[1].append(current.N2)
-        bannedNumbers[2].append(current.N3)
-        bannedNumbers[3].append(current.N4)
-    
-    new_combis_1 = []
-    for entry in combis:
-        if entry.N1 in bannedNumbers[0] or entry.N2 in bannedNumbers[1] or entry.N3 in bannedNumbers[2] or entry.N4 in bannedNumbers[3]: continue
-        if entry in playedCombis: continue
-        
-        if GetWrongPosCount(entry, current) != (jaques + mates) or GetCorrectNumCount(entry, current) != mates: 
-            continue
-        
-        new_combis_1.append(entry)
+def RemoveFromCombinations(OldCombis: list[Guess], GameHistory: list[Result]) -> list[Guess]:
+    NewCombis: list[Guess] = []
+    for num in OldCombis:
+        isCompatible: bool = True
+        for round in GameHistory:
+            if not round.IsCompatible(num): 
+                isCompatible = False
+                break
             
+        if(isCompatible): NewCombis.append(num)
         
-    return new_combis_1    
-    
-    
+    return NewCombis
+        
+def GetNextGuess(PossibleGuesses: list[Guess], GameHistory: list[Result]) -> Guess:
+    if(len(GameHistory) == 0): return Guess.FromInt(1, 2, 3, 4)
+    if(len(GameHistory) == 1): return Guess.FromInt(8, 5, 6, 7)
+    return PossibleGuesses[random.randint(0, len(PossibleGuesses)-1)]
 
-def compareNumbers(MY_GUESS: PLAY, NUM_TO_GUESS: PLAY) -> tuple[int, int]:
-    jaque = NUM_TO_GUESS.contains(MY_GUESS.N1)
-    jaque += NUM_TO_GUESS.contains(MY_GUESS.N2)
-    jaque += NUM_TO_GUESS.contains(MY_GUESS.N3)
-    jaque += NUM_TO_GUESS.contains(MY_GUESS.N4)
-
-    mate = int(MY_GUESS.N1 == NUM_TO_GUESS.N1)
-    mate += int(MY_GUESS.N2 == NUM_TO_GUESS.N2)
-    mate += int(MY_GUESS.N3 == NUM_TO_GUESS.N3)
-    mate += int(MY_GUESS.N4 == NUM_TO_GUESS.N4)
-
-    return (jaque - mate, mate)
+def PlayRound(MY_GUESS: Guess, NUM_TO_GUESS: Guess) -> Result:
+    res = Result()
+    res.Guess = MY_GUESS
+    res.CorrectNums = res.GetCorrectNums(NUM_TO_GUESS)
+    res.TotalCoincidences = res.GetTotalCoincidences(NUM_TO_GUESS)
+    return res
 
 
-def getBestGuess(combis: list[PLAY]) -> PLAY:
+def getBestGuess(combis: list[Guess]) -> Guess:
     return combis[random.randint(0, len(combis)-1)]
 
 
-def solve():
-    global playedCombis, bannedNumbers, playedResults
-    bannedNumbers = [[], [], [], []]
-    playedCombis = []
-    playedResults = []
-    NUM_TO_GUESS = getRandomNumber()
-
-    rounds: int = 0
-    combis: list[PLAY] = getPossibleCombinations()
+def solve():    
+    NUM_TO_GUESS = Guess.FromRandomGen()
+    GameHistory: list[Result] = []
+    PossibleGuesses: list[Guess] = GetPossibleCombinations()
+    CurrentGuess = GetNextGuess(PossibleGuesses, GameHistory) # Initial Guess
 
     while(True):
-        rounds += 1
-        current_guess = getBestGuess(combis)
-        (jaque, mate) = compareNumbers(current_guess, NUM_TO_GUESS)
+        result = PlayRound(CurrentGuess, NUM_TO_GUESS)
+        GameHistory.append(result)
         
-        if mate == 4:
+        if result.CorrectNums == 4:
+            if not(CurrentGuess.__eq__(NUM_TO_GUESS)): 
+                raise Exception("FUUUUUUUUUUUUUUUUUUUUUUUUUUUCK")
+            
             if PRINT_EACH_RESULT: 
-                print("Solution:", combis[0], f"(original was {NUM_TO_GUESS})", f"(done in {rounds} steps)")
-            return rounds
-                
-        playedCombis.append(current_guess)
-        playedResults.append((jaque, mate))
-        combis = getEqualOrBetterCombinations(combis, current_guess, jaque, mate)
+                print(" Solution:", CurrentGuess, f"(original was {NUM_TO_GUESS})", f"(done in {len(GameHistory)} steps)")
+            return len(GameHistory)
+              
+        PossibleGuesses = RemoveFromCombinations(PossibleGuesses, GameHistory)
+        CurrentGuess = GetNextGuess(PossibleGuesses, GameHistory)
 
-import time
 
 def to_time_str(seconds):
     mins, sec = divmod(seconds, 60)
@@ -165,23 +154,23 @@ def progressbar(it, prefix="", size=60):
         remaining = (elapsed / j) * (count - j) if j > 0 else 0
         time_str = to_time_str(remaining)
         
-        bar = f"\r{prefix}[{'#'*x}{('_'*(size-x))}] {j}/{count} Est wait {time_str}"
+        if j > 0: bar = f"\r{prefix}[{'#'*x}{('_'*(size-x))}] {j}/{count} ETA: {time_str}"
+        else: bar = f"\r{prefix}[{'#'*x}{('_'*(size-x))}] {j}/{count} ETA: N/A"
         print(bar, end="", flush=True)
         
+    show(0)
     for i, item in enumerate(it):
         yield item
         show(i+1)
-    
     print()
     
     total_time = time.time() - start    
 
-
-      
-      
 total = 0
 
 STARTTIME = time.time()
+
+print(f"Rounds: {REPS}")
 
 divider = max(int(REPS / 100), 1)
 
